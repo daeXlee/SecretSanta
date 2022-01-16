072bb4bd33545a410df21d46de0d1a03134aed41
daeXlee <daewonxlee@gmail.com>
2022-01-16 14:57:03 -0600

    Added Results

--- app.js
+++ app.js
@@ -6,20 +6,43 @@ let nameInput = document.getElementById("nameInput");
 let participantsDisplay = document.getElementById("participantsDisplay");
 let pairsDisplay = document.getElementById("pairsDisplay");
 
-function displayNames(namesToDisplay, diplayDiv) {
-    let displayHTML = '<ul>';
-    for (let name of namesToDisplay) {
-        displayHTML += '<li>' + name +'</li>';
-    }
+function displayNames() {
+    participantsDisplay.innerHTML = '';
+    let unorderList = document.createElement('ul');
+    participantsDisplay.appendChild(unorderList);
 
-    displayHTML += '</ul>';
-    diplayDiv.innerHTML = displayHTML;
+    for (let name of participants) {
+        let listElement = document.createElement('li');
+        listElement.innerText = name;
+        unorderList.appendChild(listElement);
+    }
 }
 
 function addName() {
     participants.push(nameInput.value);
     nameInput.value = '';
-    displayNames(participants, participantsDisplay);
+    displayNames();
+}
+
+function displayResults() {
+    pairsDisplay.innerHTML = '';
+    let unorderList = document.createElement('ul');
+    pairsDisplay.appendChild(unorderList);
+
+    for (let pair of pairs) {
+        let listElement = document.createElement('li');
+        unorderList.appendChild(listElement);
+
+        let a = document.createElement('a');
+        listElement.appendChild(a);
+
+        let linkText = document.createTextNode(pair[0]);
+        a.appendChild(linkText);
+        let pathQueryString = '?name=' + encodeURIComponent(pair[0]) + '&pair=' + encodeURIComponent(pair[1]);
+        a.title = pair[0];
+        a.href = './results.html' + pathQueryString;
+        a.target = '_blank';
+    }
 }
 
 // Pseudo Linked List pairing its neighbor and last gets first
@@ -30,5 +53,6 @@ function calculatePairs() {
         pairs.push([shuffled[i], shuffled[i+1]]);
     }
     pairs.push([shuffled.pop(), shuffled[0]]);
-    displayNames(pairs, pairsDisplay);
+
+    displayResults();
 }
